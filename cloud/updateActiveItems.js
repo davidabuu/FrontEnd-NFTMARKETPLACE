802ba9c95f9bc7797323787c2d72d42b7524b0c8
ABU DAVID <65656238+davidabuu@users.noreply.github.com>
2022-09-12 00:57:10 +0100

    Events Added

--- cloud/updateActiveItems.js
+++ cloud/updateActiveItems.js
@@ -1,14 +1,51 @@
 const { default: Moralis } = require("moralis/types")
 
-
 Moralis.Cloud.afterSave("ItemListed", async (request) => {
     const confirmed = request.object.get("confirmed")
     const logger = Moralis.Cloud.getLogger()
     logger.info("Looking for confimed Tx")
-    if(confirmed){
-        logger.info('Found Them')
-        const ActiveItems = Moralis.Object.extend('ActiveItem')
+    if (confirmed) {
+        logger.info("Found Them")
+        const ActiveItems = Moralis.Object.extend("ActiveItem")
         const activeItem = new ActiveItems()
-        activeItem.set('marketplaceAddress', re )
+        activeItem.set("marketplaceAddress", request.object.get("address"))
+        activeItem.set("nftAddress", request.object.get("nftAddress"))
+        activeItem.set("price", request.object.get("price"))
+        activeItem.set("tokenId", request.object.get("tokenId"))
+        activeItem.set("seller", request.object.get("seller"))
+        logger.info("Adding Address")
+        logger.info('Saving....')
+        await activeItem.save()
+    }
+})
+
+Moralis.Cloud.afterSave("ItemCanceled", async (request) => {
+    const confirmed = request.object.get("confirmed")
+    const logger = Moralis.Cloud.getLogger()
+    logger.info(`Marketplace | Object: ${request.object}`)
+    if (confirmed) {
+        const ActiveItem = Moralis.Object.extend("ActiveItem")
+        const query = new Moralis.Query(ActiveItem)
+        query.equalTo("marketplaceAddress", request.object.get("address"))
+        query.equalTo("nftAddress", request.object.get("nftAddress"))
+        query.equalTo("tokenId", request.object.get("tokenId"))
+        logger.info(`Marketplace | Query: ${query}`)
+        const canceledItem = await query.first()
+        logger.info(`Marketplace | CanceledItem: ${JSON.stringify(canceledItem)}`)
+        if (canceledItem) {
+            logger.info(`Deleting ${canceledItem.id}`)
+            await canceledItem.destroy()
+            logger.info(
+                `Deleted item with tokenId ${request.object.get(
+                    "tokenId"
+                )} at address ${request.object.get("address")} since it was canceled. `
+            )
+        } else {
+            logger.info(
+                `No item canceled with address: ${request.object.get(
+                    "address"
+                )} and tokenId: ${request.object.get("tokenId")} found.`
+            )
+        }
     }
 })
