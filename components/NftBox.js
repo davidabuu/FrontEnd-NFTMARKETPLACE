5c941e65e8a92ab9f065abe5c17405cb9a1481e6
ABU DAVID <65656238+davidabuu@users.noreply.github.com>
2022-09-12 12:54:32 +0100

    Query Moralis Data

---
+++ components/NftBox.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NftBox = ({price, nftAddress, tokenId, seller}) => {
+  return (
+    <div></div>
+  )
+}
+
+export default NftBox
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,24 @@
 import Head from "next/head"
+import { useMoralisQuery } from "react-moralis"
 export default function Home() {
+    const {data: listedNfts, isFetching: fetchingListedNfs} = useMoralisQuery(
+        'ActiveItem',
+        (query) => query.limit(10).descending('tokenId')
+    )
     return (
+      
         <div>
             <Head>
                 <title>NFT Marketplace</title>
                 <meta name="description" content="Nft Marketplace" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-          
+          {fetchingListedNfs ? <div>Loading....</div> : listedNfts.map((nft) => {
+            const {price, nftAddress, tokenId, marketplaceAddress, seller} = nft.attributes
+            return(
+                <div>Price: {price}, NftAddress: {nftAddress}. TokenId:{tokenId}, Seller: {seller}</div>
+            )
+})}
         </div>
     )
 }
